614b51833d73153b4c4bab6869ac0079a69e9b3a
greykurotsuki <50923540+greykurotsuki@users.noreply.github.com>
2021-11-01 16:04:58 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'aws-codedeploy',
+      name: 'Portfolio-master',
       script: 'npx',
       args: 'serve -s build -l 3000 -n',
       interpreter: 'none',
